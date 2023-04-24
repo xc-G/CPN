41215b1c3f57ba4570f86c503088637a04166006
GaoXC <31365303+xc-G@users.noreply.github.com>
2023-04-24 17:04:18 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=23445 --use_env train_mobilenet.py --data-set IMNET --dist-eval --output mobilenetv2_0.25_dyreluA_k2_bs512_lr0.1_sgd_cositer_weightdecay4e-5 --batch-size 128 --model mobilenet --lr 0.1
+CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=23445 --use_env train_mobilenet.py --data-set IMNET --dist-eval --output output_path --batch-size 128 --model mobilenet --lr 0.1
