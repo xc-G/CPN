5de45e8634fd7d268eb547a49afc25ecb99a4091
GaoXC <31365303+xc-G@users.noreply.github.com>
2023-04-28 14:16:27 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --nproc_per_node=4 --master_port=23445 --use_env train_mobilenet.py --data-set IMNET --dist-eval --output output_path --batch-size 128 --model mobilenet --lr 0.1
